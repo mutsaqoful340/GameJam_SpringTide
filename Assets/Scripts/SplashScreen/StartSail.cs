630e1073603bf681c4e600e5b984937be2894883
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-09-11 12:35:12 +0700

    Updatesfx

--- Assets/Scripts/SplashScreen/StartSail.cs
+++ Assets/Scripts/SplashScreen/StartSail.cs
@@ -3,13 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class StartSail : MonoBehaviour
 {
+    [Header("SFX")]
+    public AudioClip clickSFX; // drag clip klik ke sini lewat Inspector
+
     public void StartGame()
     {
-        SceneManager.LoadScene("MAINMENU"); // ganti dengan nama scene game kamu
+        // mainkan sound effect klik
+        if (SFXPlayer.Instance != null && clickSFX != null)
+        {
+            SFXPlayer.Instance.PlaySFX(clickSFX);
+        }
+
+        // lalu ganti scene
+        SceneManager.LoadScene("MAINMENU");
     }
 
     public void QuitGame()
     {
+        // mainkan sound effect klik
+        if (SFXPlayer.Instance != null && clickSFX != null)
+        {
+            SFXPlayer.Instance.PlaySFX(clickSFX);
+        }
+
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
