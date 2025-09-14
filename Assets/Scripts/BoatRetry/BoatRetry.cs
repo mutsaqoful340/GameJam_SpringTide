150414ed8cc7f93fff3985eb253b4cb6c14775b2
Fikri Ahmad <rukimevick@gmail.com>
2025-09-14 10:27:54 +0700

    Create Boat Retry.cs

---
+++ Assets/Scripts/BoatRetry/BoatRetry.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatRetry : MonoBehaviour
+{
+    private BoatDurability boatDurability;
+    private BoatFlipChecker boatFlipChecker;
+    private BoatMovement boatMovement;
+    private MarineHorn marineHorn;
+    private EOTCycler eotCycler;
+}
